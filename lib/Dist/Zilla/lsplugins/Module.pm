ad67a8d9808271a0db37a6e8c2a391b98d357217
Kent Fredric <kentfredric@gmail.com>
2014-08-01 20:08:30 +1200

    Build results of 7559538 (on master)

--- lib/Dist/Zilla/lsplugins/Module.pm
+++ lib/Dist/Zilla/lsplugins/Module.pm
@@ -11,8 +11,8 @@ our $VERSION = '0.001001';
 
 our $AUTHORITY = 'cpan:KENTNL'; # AUTHORITY
 
-use Moose;
-use Try::Tiny;
+use Moose qw( has );
+use Try::Tiny qw( try catch );
 
 has 'file'             => ( is => ro =>, required   => 1 );
 has 'plugin_root'      => ( is => ro =>, required   => 1 );
